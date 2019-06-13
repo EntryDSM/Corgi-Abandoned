b647240f9d3426127e5777823d805f57e936bca4
HubCodes <krlrhkstk12@gmail.com>
2019-06-14 08:58:59 +0900

    Remove meaningless defer function from main

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"log"
-
 	"github.com/EntryDSM/Corgi/initialize"
 	"github.com/EntryDSM/Corgi/loop"
 )
 
 func main() {
-	defer writeFatalLog()
-
 	initialize.Initialize()
 	loop.StartLoop()
-}
-
-func writeFatalLog() {
-	if err := recover(); err != nil {
-		log.Fatal(err)
-	}
 }
